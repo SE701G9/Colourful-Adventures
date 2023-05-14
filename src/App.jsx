ebd93c86dc9f73212d631fe18b538584f426bfcd
Jason Ko <jekb2019@gmail.com>
2023-05-14 18:41:13 +1200

    Remove unnecessary files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        Project Setup for SE701 G9 - Colourful Adventures
+      </h1>
+    </div>
+  );
+}
+
+export default App;
