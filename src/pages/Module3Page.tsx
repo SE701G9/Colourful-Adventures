36d4a22419b49405cced9fb9838938fad94f48b0
oeng099 <oeng099@aucklanduni.ac.nz>
2023-05-30 21:29:59 +1200

    Implemented OneStarPopUp component

--- src/pages/Module3Page.tsx
+++ src/pages/Module3Page.tsx
@@ -1,12 +1,13 @@
 import MainLayout from "../components/layouts/MainLayout";
 import NoStarsPopUp from "../components/rewards/NoStarsPopUp";
+import OneStarPopUp from "../components/rewards/OneStarPopUp";
 
 
 export default function Module3Page() {
   return (
     <MainLayout>
       <h1>Third module: Colour Concepts</h1>
-      <NoStarsPopUp/>
+      <OneStarPopUp/>
     </MainLayout>
   )
 }
