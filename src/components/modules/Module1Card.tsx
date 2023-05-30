6fce34bb2909165702f00ae97fb80ebaacd61002
imet057 <imet057@aucklanduni.ac.nz>
2023-05-30 16:07:25 +1200

    Component resizing for laptop screen

--- src/components/modules/Module1Card.tsx
+++ src/components/modules/Module1Card.tsx
@@ -6,6 +6,8 @@ import Typography from '@mui/material/Typography';
 import CardMedia from '@mui/material/CardMedia';
 import { Link } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
+import { useMediaQuery } from 'react-responsive'
+import './Module1Card.css';
 
   export default function ModuleCard() {
 
@@ -15,18 +17,18 @@ import { styled } from '@mui/material/styles';
 
     return (
       <Link to={"/modules/1"}>
-        <StyledCard className={"module-1-card"} sx={{ width: 500, height: 650, borderRadius: 5, boxShadow: 5, border: 4, backgroundColor: "#FFCE51" }} >
+        <StyledCard className={"module-1-card"} sx={{ width: 400, height: 500,  borderRadius: 5, boxShadow: 5, border: 4, backgroundColor: "#FFCE51" }} >
           <CardContent>
           <div className="level-title">
-            <Typography align='center' sx={{fontSize: 60, fontFamily: 'Jeju Hallasan'}}>
+            <Typography align='center' sx={{fontSize: 50, fontFamily: 'Jeju Hallasan'}}>
               Level 1
             </Typography>
           </div>
           <div className="module-title">
-            <Typography align='center' sx={{fontSize: 40, fontFamily: 'Jeju Hallasan'}}>
+            <Typography align='center' sx={{fontSize: 30, fontFamily: 'Jeju Hallasan'}}>
               Basic
             </Typography>
-            <Typography align='center' sx={{fontSize: 40, fontFamily: 'Jeju Hallasan'}}>
+            <Typography align='center' sx={{fontSize: 30, fontFamily: 'Jeju Hallasan'}}>
               Colour Wheel
             </Typography>
           </div>
@@ -35,7 +37,7 @@ import { styled } from '@mui/material/styles';
           className={"basic-colour-wheel-image"}
           image={require('../../images/navigation-images/basic_colour_wheel.png')}
           title="Basic colour wheel image"
-          sx={{height: 270, width: 300, objectFit: "contain", marginLeft: 12, marginTop: 6}}
+          sx={{height: 220, width: 250, objectFit: "contain", marginLeft: 8, marginTop: 5}}
         />
         <CardActions>
         </CardActions>
