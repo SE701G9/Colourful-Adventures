b8d8ed93449cdbccd4349c572128ecb423b2bf63
oeng099 <oeng099@aucklanduni.ac.nz>
2023-05-30 21:34:01 +1200

    Implemented ThreeStarPopUp component

--- src/pages/Module3Page.tsx
+++ src/pages/Module3Page.tsx
@@ -1,6 +1,7 @@
 import MainLayout from "../components/layouts/MainLayout";
 import NoStarsPopUp from "../components/rewards/NoStarsPopUp";
 import OneStarPopUp from "../components/rewards/OneStarPopUp";
+import ThreeStarPopUp from "../components/rewards/ThreeStarPopUp";
 import TwoStarPopUp from "../components/rewards/TwoStarPopUp";
 
 
@@ -8,7 +9,7 @@ export default function Module3Page() {
   return (
     <MainLayout>
       <h1>Third module: Colour Concepts</h1>
-      <TwoStarPopUp/>
+      <ThreeStarPopUp/>
     </MainLayout>
   )
 }
