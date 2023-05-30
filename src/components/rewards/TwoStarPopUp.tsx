3d778cf1a95fbb47deeaf83fa7dc7734ee97cde5
oeng099 <oeng099@aucklanduni.ac.nz>
2023-05-30 21:31:52 +1200

    Implemented TwoStarPopUp component

---
+++ src/components/rewards/TwoStarPopUp.tsx
@@ -0,0 +1,21 @@
+import Stars from "./Stars";
+import './NoStarsPopUp.css';
+
+type TwoStarPopUpProps = {
+      children: React.ReactNode;
+}
+
+const TwoStarPopUp = () => {
+
+      return(
+      <div className = "reward-layout">
+            <Stars
+                  src = {require('../../images/reward-images/twoStar.png')}
+                  alt = {"No stars"}
+                  />
+            <h1> Default Text Stuff</h1>
+      </div>
+      );
+}
+
+export default TwoStarPopUp;
--- src/pages/Module3Page.tsx
+++ src/pages/Module3Page.tsx
@@ -1,13 +1,14 @@
 import MainLayout from "../components/layouts/MainLayout";
 import NoStarsPopUp from "../components/rewards/NoStarsPopUp";
 import OneStarPopUp from "../components/rewards/OneStarPopUp";
+import TwoStarPopUp from "../components/rewards/TwoStarPopUp";
 
 
 export default function Module3Page() {
   return (
     <MainLayout>
       <h1>Third module: Colour Concepts</h1>
-      <OneStarPopUp/>
+      <TwoStarPopUp/>
     </MainLayout>
   )
 }
