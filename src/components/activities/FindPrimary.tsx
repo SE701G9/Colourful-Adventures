414abd67da9442e9286f74ce864fe136fd458209
Matthew Liu <mliu996@aucklanduni.ac.nz>
2023-05-30 20:29:20 +1200

    updated buttons, and glowing effects

--- src/components/activities/FindPrimary.tsx
+++ src/components/activities/FindPrimary.tsx
@@ -3,6 +3,7 @@ import DialogueMenu from '../dialogue/DialogueMenu';
 import './FindPrimary.css';
 import '../dialogue/DialogueBox.css';
 import { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 
@@ -53,18 +54,30 @@ const FindPrimary = () => {
     console.log('Blue Blanket Clicked');
   }
   
+  const itemWorms = new URL("/src/images/item-images/red_worms.png", import.meta.url)
+  const itemKeys = new URL("/src/images/item-images/yellow_keys.png", import.meta.url)
+  const itemBlanket = new URL("/src/images/item-images/blue_blanket.png", import.meta.url)
+
   return (
     <MainLayout>
-      <main className="find-primary-container">
-        <DialogueMenu
-          name = {dialogue.name}
-          message = {dialogue.message}
-          onclick = {changeDialogueData}>
-        </DialogueMenu>
-        <button onClick={handleWorm}>Red Worm</button>
-        <button onClick={handleKey}>Yellow Key</button>
-        <button onClick={handleBlanket}>Blue Blanket</button>
-      </main>
+      <div className='bg'>
+        <main className="find-primary-container">
+          <DialogueMenu
+            name = {dialogue.name}
+            message = {dialogue.message}
+            onclick = {changeDialogueData}>
+          </DialogueMenu>
+          <button className="redWorm" onClick={handleWorm}>
+            <img src = {String(itemWorms)} />
+          </button>
+          <button className="yellowKeys" onClick={handleKey}>
+            <img src = {String(itemKeys)} />
+          </button>
+          <button className="blueBlanket" onClick={handleBlanket}>
+            <img src = {String(itemBlanket)} />
+          </button>
+        </main>
+      </div>
     </MainLayout>
   )
 }
