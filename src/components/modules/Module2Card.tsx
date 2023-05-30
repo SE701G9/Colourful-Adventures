124b0cb26bc1c6e09e39c987d16c2b7c6b8f38d3
mliu996 <87844385+mliu996@users.noreply.github.com>
2023-05-30 22:09:47 +1200

    Update Module2Card.tsx

--- src/components/modules/Module2Card.tsx
+++ src/components/modules/Module2Card.tsx
@@ -6,7 +6,6 @@ import Typography from '@mui/material/Typography';
 import { CardMedia } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
-import { useMediaQuery } from 'react-responsive'
 import './Module2Card.css';
 
 
