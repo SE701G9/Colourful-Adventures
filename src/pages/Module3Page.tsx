de7889461c729f72472b8327782608fe0ae594bb
Owen Eng <oeng099@aucklanduni.ac.nz>
2023-05-30 18:07:42 +1200

    Added back button to all module pages

--- src/pages/Module3Page.tsx
+++ src/pages/Module3Page.tsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import BackButton from "../components/navigation/BackButton";
 
 
 export default function Module3Page() {
+  const navigate = useNavigate();
+
+  const goBack= () => {
+    navigate('/modules')
+  };
   return (
     <div>
+    <BackButton
+        onClick={goBack}
+        />
       <h1>Third module: Colour Concepts</h1>
     </div>
   )
